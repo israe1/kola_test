bc86cda447dfd19b31fe1c542feb0d744980088d
Israël Mekomou <israelmekomou@gmail.com>
2021-04-16 19:11:48 +0100

    app_design

---
+++ app/src/main/java/com/israel/kola/CreateGoalActivity.kt
@@ -0,0 +1,11 @@
+package com.israel.kola
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateGoalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_goal)
+    }
+}
---
+++ app/src/main/java/com/israel/kola/GoalDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.israel.kola
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoalDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goal_detail)
+    }
+}
